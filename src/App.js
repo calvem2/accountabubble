a5946bc8c11afd0f5e527849ba3bf8330d055d02
clairebeard <beard.claire@hotmail.com>
2021-01-16 14:00:53 -0800

    Added new features and styled the week view page.
    Added a logo and other functionality to the weekly view page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Bubble from "./Bubble";
 import ProgressBar from "./ProgressBar";
 import WeekDay from "./WeekDay";
+import logo from "./logo.png";
 
 function App() {
+  // Title of the web page tab
+  document.title = "accountabubble";
+  // Hard coded tasks
   let tasks = [{name:"yoga", assignee:"ash", day:"monday", completed:false}, 
               {name:"legos", assignee:"claire", day:"sunday", completed:true},
               {name:"plants", assignee:"megan", day:"monday", completed:true},
-              {name:"make pun", assignee:"ash", day:"monday", completed:false}];
+              {name:"make pun", assignee:"ash", day:"monday", completed:false},
+              {name:"make pun", assignee:"ash", day:"saturday", completed:true},
+              {name:"british accent", assignee:"claire", day:"saturday", completed:true},
+              {name:"roast claire", assignee:"megan", day:"saturday", completed:true}];
   let days = [];
+  // Weekdays
   let weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+  // Get the current day
+  let now = new Date();
+  let currDay = now.getDay() == 0 ? weekdays[6] : weekdays[now.getDay() - 1];
+
   for (let i = 0; i < weekdays.length; i++) {
     days.push(
-      <WeekDay day={weekdays[i]} tasks={tasks.filter(task => {
+      <WeekDay profileName={"megan"} isCurrDay={currDay === weekdays[i]} day={weekdays[i]} tasks={tasks.filter(task => {
         return task.day === weekdays[i];
       })}/>
     )
   }
+
   return (
     <div className="App">
+      <img src={logo} alt="Accountabubble logo"/>
      {/* Add logo here */}
      <h2>accountabubble</h2> 
      <ProgressBar tasks={tasks}/>
--- src/WeekDay.js
+++ src/WeekDay.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-
+let cx = require('classnames');
 
 class WeekDay extends Component {
 
     render() {
-        let dailyTasks = [];
-        for (let i = 0; i < this.props.tasks.length; i++) {
-            dailyTasks.push(<div class="tasks">{this.props.tasks[i].name}</div>);
+        // Filter tasks based on assignees
+        let assignees = [...new Set(this.props.tasks.map(task => task.assignee))];
+        assignees.splice(assignees.indexOf(this.props.profileName), 1);
+        if (assignees.length > 0) {
+            assignees.unshift(this.props.profileName);
         }
+
+        let dailyTasks = [];
+        for (let i = 0; i < assignees.length; i++) {
+            dailyTasks.push(
+                <div class="task_group">
+                    <div class="assignee">{
+                        assignees[i] === this.props.profileName ? "your tasks" : assignees[i]
+                    }</div>
+                    {this.props.tasks.filter(task => {
+                        return task.assignee === assignees[i];
+                    }).map(task => <div class="tasks">{task.name}</div>)}
+                </div>
+            );
+        }
+        let classNames = cx({
+            'week-days': true,
+            'selected-day': this.props.isCurrDay
+        })
+
+        console.log(this.props.isCurrDay);
         return (
-            <div class="week_days">
+            <div className={classNames}>
                 <h3>{this.props.day}</h3>
-                {/* Add the names here. Sort tasks based on name. */}
                 {dailyTasks}
+                <div className="add-task">+ add task</div>
             </div>
         );
     }
